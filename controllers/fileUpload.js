482c49b63c7ce06cf7427ea0262b0d295e56281f
aditya10-10 <ascs25835@gmail.com>
2023-12-18 10:38:52 +0530

    imageupload videoupload imageresizer working good

--- controllers/fileUpload.js
+++ controllers/fileUpload.js
@@ -36,9 +36,15 @@ function isFileSupported(fileType, supportedTypes) {
   return supportedTypes.includes(fileType);
 }
 
-async function uploadFile(file, folder) {
-  const options = { folder };
-  await cloudinary.uploader.upload(file.tempFilePath, options);
+async function uploadFile(file, folder, quality) {
+  const options = { folder, max_file_size: 1024 };
+
+  if (quality) {
+    options.quality = quality;
+  }
+
+  options.resource_type = "auto";
+  return await cloudinary.uploader.upload(file.tempFilePath, options);
 }
 
 exports.imageUpload = async (req, res) => {
@@ -63,18 +69,112 @@ exports.imageUpload = async (req, res) => {
     }
 
     const response = await uploadFile(file, "File_upload");
+    console.log(response);
 
-    // const fileData = await File.create({
-    //   name,
-    //   tags,
-    //   email,
-    //   imageUrl,
-    // })
+    const fileData = await File.create({
+      name,
+      tags,
+      email,
+      imageUrl: response.secure_url,
+    });
 
     res.status(200).json({
       success: true,
       message: "File Uploaded Successfully",
       data: response,
+      imageUrl: response.secure_url,
+    });
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({
+      success: false,
+      message: "Server Error",
+    });
+  }
+};
+
+exports.videoUpload = async (req, res) => {
+  try {
+    const { name, tags, email } = req.body;
+    console.log("body", req.body);
+
+    const file = req.files.videoFile;
+    // console.log("file", file);
+
+    const supportedTypes = ["mov", "mp4"];
+
+    const fileType = file.name.split(".")[1].toLowerCase();
+
+    console.log("fileType", fileType);
+
+    if (!isFileSupported(fileType, supportedTypes)) {
+      return res.status(400).json({
+        success: false,
+        message: "File type not supported",
+      });
+    }
+
+    const response = await uploadFile(file, "File_upload");
+    console.log(response);
+
+    const fileData = await File.create({
+      name,
+      tags,
+      email,
+      imageUrl: response.secure_url,
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "Video Uploaded Successfully",
+      data: response,
+      imageUrl: response.secure_url,
+    });
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({
+      success: false,
+      message: "Server Error",
+    });
+  }
+};
+
+exports.imageSizeReducer = async (req, res) => {
+  try {
+    const { name, tags, email } = req.body;
+    console.log("body", req.body);
+
+    const file = req.files.imageFile;
+    // console.log("file", file);
+
+    const supportedTypes = ["png", "jpg", "jpeg"];
+
+    const fileType = file.name.split(".")[1].toLowerCase();
+
+    console.log("fileType", fileType);
+
+    if (!isFileSupported(fileType, supportedTypes)) {
+      return res.status(400).json({
+        success: false,
+        message: "File type not supported",
+      });
+    }
+
+    const response = await uploadFile(file, "File_upload", 40);
+    console.log(response);
+
+    const fileData = await File.create({
+      name,
+      tags,
+      email,
+      imageUrl: response.secure_url,
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "File Uploaded Successfully",
+      data: response,
+      imageUrl: response.secure_url,
     });
   } catch (e) {
     console.error(e);

1e46b79c93743693c2e1286a569099583ab257a4
aditya10-10 <ascs25835@gmail.com>
2023-12-19 11:33:47 +0530

    nodemailer working good

--- models/Files.js
+++ models/Files.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const nodemailer = require("nodemailer");
+require("dotenv").config();
 
 const fileSchema = new mongoose.Schema({
   name: {
@@ -19,14 +21,30 @@ const fileSchema = new mongoose.Schema({
   },
 });
 
-// fileSchema.post("save", async function (doc) {
-//   try {
-//     console.log("DOC", doc);
-//   } catch (error) {
-//     console.log(error);
-//     process.exit(1);
-//   }
-// });
+fileSchema.post("save", async function (doc) {
+  try {
+    console.log("DOC", doc);
+
+    let transporter = nodemailer.createTransport({
+      host: process.env.MAIL_HOST,
+      port: "587",
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
+      },
+    });
+
+    let info = await transporter.sendMail({
+      from: "Aditya Chauhan",
+      to: doc.email,
+      subject: "Document saved successfully to Cloudinary",
+      html: `<h2>Post Upload</h2>  <p>File Uploaded view here: <a href = "${doc.imageUrl}">link</a></p> `,
+    });
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+});
 
 const File = mongoose.model("File", fileSchema);
 module.exports = File;
